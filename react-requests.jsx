aaf7693a73ed0e737827589899ca5414dfe24e85
cblanc <cablanchard@gmail.com>
2014-09-09 11:07:44 +0100

    Added protocol support

--- react-requests.jsx
+++ react-requests.jsx
@@ -3,15 +3,30 @@
 (function (window, React) {
 	"use strict";
 
+	if (window.jQuery) {
+		var $ = window.jQuery;
+	} else {
+		return console.error("jQuery is required for the ReactRequest component");
+	}
+
 	var ReactRequest = React.createClass({
+		getDefaultProps: function() {
+	    return {
+	      protocol: "http",
+	      targetUrl: ""
+	    };
+	  },
 		getInitialState: function () {
 			return { 
 				data: ""
-			}
+			};
+		},
+		url: function () {
+			return [this.props.protocol,"://",this.props.targetUrl].join("");
 		},
 		handleClick: function (event) {
 			this.setState({data: ""});
-			$.ajax(this.props.targetUrl, {
+			$.ajax(this.url(), {
 				success: function (data) {
 					this.setState({data: JSON.stringify(data.result, 2, 2)})
 				}.bind(this)
@@ -20,7 +35,7 @@
 		render: function () {
 			return (
 				<div className="reactRequest">
-					<h3 className="reactRequst-url">{this.props.targetUrl}</h3>
+					<h3 className="reactRequst-url">{this.url()}</h3>
 					<a className="reactRequest-button" onClick={this.handleClick}>Request</a>
 					<pre>{this.state.data}</pre>
 				</div>
@@ -28,10 +43,10 @@
 		}
 	});
 
-
 	if (typeof module === 'undefined') {
     window.ReactRequest = ReactRequest;
   } else {
     module.exports = TreeView;
   }
+
 })(window, typeof require === 'function' ? require('react') : React);
